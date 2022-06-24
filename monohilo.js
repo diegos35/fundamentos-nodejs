266270786a51ce796a2e7579227cc5be315a10ba
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-06-23 23:16:13 -0500

    feat: trabajando con node variables de entorno, instalando nodemon

---
+++ monohilo.js
@@ -0,0 +1,17 @@
+console.log('hello world');
+
+let i = 0;
+
+setInterval(function() {
+    console.log(i);
+    i++;
+    try {
+        if (i === 5) {
+            var a = 3 + z;
+        }
+    } catch (error) {
+        console.error(error)
+    }
+},  1000);
+
+console.log('segunda instructions');
---
+++ readme.md
@@ -0,0 +1,7 @@
+execute
+$ node monohilo.js
+
+install Nodemon
+https://nodemon.io :  npm install -g nodemon
+
+run nodemon: $ nodemon .\monohilo.js
