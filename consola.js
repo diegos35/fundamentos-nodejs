aea44f4a703b3c64726f2e57c419bbbbdc6a5f52
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-07-12 00:46:31 -0500

    feat: consola

---
+++ consola.js
@@ -0,0 +1,17 @@
+console.log("algo");
+console.info("info");
+console.error("error");
+console.warn("warn");
+
+var table = [
+    {
+        a: 1,
+        b:2
+    },
+    {
+        a: 2,
+        b: 'Other key'
+    }
+]
+
+console.table(table);
